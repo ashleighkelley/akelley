b9fd1d2297dd4191ca41df4e280158674282a2f0
Ashleigh Kelley <31831238+ashleighkelley@users.noreply.github.com>
2024-01-31 10:55:23 -0500

    API updates
    Don't use route handlers with api routes?

--- app/interests/scrobbles/api/albums/route.js
+++ app/interests/scrobbles/api/albums/route.js
@@ -1,5 +1,3 @@
-import { NextResponse } from "next/server";
-
 export async function GET() {
 
     try {
@@ -7,10 +5,10 @@ export async function GET() {
       const res = await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&period=3month&user=shleeeeigh&api_key=${process.env.LASTFM_API_KEY}&format=json`);
       const data = await res.json();
       
-      return NextResponse.json({ data: data },{status:201});
+      return Response.json({ data: data },{status:201});
 
     } catch (err) {
-      return NextResponse.json({ error: 'Internal Server Error' },{status:500});
+      return Response.json({ error: 'Internal Server Error' },{status:500});
     }
 
   };
--- app/interests/vinyl/api/albums/route.js
+++ app/interests/vinyl/api/albums/route.js
@@ -1,5 +1,3 @@
-import { NextResponse } from "next/server";
-
 export async function GET() {
 
     try {
@@ -7,10 +5,10 @@ export async function GET() {
       const res = await fetch('https://api.discogs.com/users/kelleya92/collection/folders/0/releases&format=json');
       const data = await res.json();
       
-      return NextResponse.json({ data: data },{status:201});
+      return Response.json({ data: data },{status:201});
 
     } catch (err) {
-      return NextResponse.json({ error: 'Internal Server Error' },{status:500});
+      return Response.json({ error: 'Internal Server Error' },{status:500});
     }
 
   };
